f424dfad7079edc3d57826769b40288f6ab55bc4
Andrew Alagna <aalagna04@gmail.com>
2022-06-05 11:13:44 -0400

    refactor: project data

--- src/data.js
+++ src/data.js
@@ -3,16 +3,17 @@ export const projects = [
     title: "Invent0ry",
     subtitle: "React and AWS (Amplify)",
     description:
-      "This is to help businesses keep track of their inventory across different storage locations. We used Amplify from AWS for our database + API, authentication, and hosting.",
+      "This app helps businesses keep track of their inventory across different storage locations. We used Amplify from AWS for authentication, hosting, and for our database & API.",
     image:
       "https://user-images.githubusercontent.com/40577932/168584742-5f09d0d2-5683-46b3-9393-748f5dad6a89.gif",
     link: "https://main.d2lpf19m4xl42s.amplifyapp.com/",
   },
   {
     title: "CunyFirst Remade",
-    subtitle: "React + Express + PostgreSQL",
+    subtitle: "React + Express + PostgreSQL + Firebase",
     description:
-      "This app helps you keep track of teachers and classes that your taking. I used MaterialUI for styling components making it responive to mobile and desktop web browsers.",
+      "This app helps you keep track of teachers and courses while in school. " +
+        "I used MaterialUI for styling components making it responive to mobile and desktop web browsers.",
     image:
       "https://user-images.githubusercontent.com/40577932/168820750-f430fb37-d758-4d7c-b0d2-0e4498756b20.gif",
     link: "https://elchic00.github.io/CunyFirst-front/",
@@ -21,7 +22,8 @@ export const projects = [
     title: "Multi-Purpose Calculator",
     subtitle: "Python (PyQT5)",
     description:
-      "Multi-Purpose Calculator is a stylish calculator app that features an on-sceen calculator and converter. This is to be used on a desktop with Python installed.",
+      "Multi-Purpose Calculator is a stylish calculator app that features an on-sceen calculator and converter." +
+        "This is to be used on a desktop with Python installed.",
     image:
       "https://github.com/NesQuickCoding/Multi-Purpose-Calculator/raw/main/demo/Demo.gif?raw=true",
     link: "https://github.com/NesQuickCoding/Multi-Purpose-Calculator",
@@ -30,7 +32,8 @@ export const projects = [
     title: "Crime in Queens NYC",
     subtitle: "Python, HTML, Github MD",
     description:
-      "Finding the trend of crime in Queens NYC. This project uses Python data-science libraries to process the data. The web-page was created using html and Github MD.",
+      "Finding the trend of crime in Queens NYC. This project uses Python data-science libraries to process the data. " +
+        "The web-page was created using html and Github MD.",
     image: "https://elchic00.github.io/CrimeInQueens/crimesByPrec.png",
     link: "https://elchic00.github.io/CrimeInQueens",
   },
@@ -38,7 +41,8 @@ export const projects = [
     title: "Macros-for-geeks",
     subtitle: "Angular, .Net, SQLite",
     description:
-      "A food diary app to help users keep track of their macronutrients. They can save their diary to a local SQLite database, which they can later view whether online or not.",
+      "A food diary app to help users keep track of their macronutrients. " +
+        "They can save their diary to a local SQLite database, which they can later view whether online or not.",
     image: "https://github.com/elchic00/Macros-for-geeks/raw/main/macros.gif",
     link: "https://github.com/elchic00/Macros-for-geeks",
   },
@@ -58,12 +62,12 @@ export const skills = [
   "JavaScript (ReactJS, TypeScript)",
   "C#",
   "C++",
-  "SQL",
+  "SQL (SQLite, MySQL, PostgreSQL)",
   "NoSQL",
-  "Kali Linux", 
-  "Burp-suite",
   "AWS",
-  "Firebase"
+  "Firebase",
+   "Kali Linux",
+   "Burp-suite",
 ];
 // export const testimonials = [
 // ];
