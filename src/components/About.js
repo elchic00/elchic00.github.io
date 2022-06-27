347f3c531b4ba1a9564a71d8c7f07bd2eb34a3cc
Andrew Alagna <aalagna04@gmail.com>
2022-06-27 12:15:01 -0400

    refactor: about buttons and project desc

--- src/components/About.js
+++ src/components/About.js
@@ -9,101 +9,96 @@ export default function About() {
   const navigate = useNavigate();
 
   return (
-      <>
-    <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center mt-10">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white mt-5 ">
-            Hi, my name is Andrew Alagna.
-          </h1>
-          <p className="mb-7 leading-relaxed">
-            A motivated student of computer science, interested in working with people who
-            want to solve meaningful problems with software engineering.
-            <br />
-            <br />
-            I enjoy learning about technology and find pride in solving problems.
-            <br />
-            <br />
-            Graduated from CUNY: Hunter College with a BA in Computer Science, Cum Laude.
-          </p>
-          <ul className="flex lg:flex-row lg:space-x-4 flex-col list-none mb-5">
-            <a
-              href="https://www.github.com/elchic00/"
-              rel="noreferrer"
-              target="_blank"
-              className="mb-6 inline-flex items-center h-10 p-5 text-gray-300 bg-gray-800 hover:bg-gray-700 duration-500 transition-colors rounded focus:shadow-outline hover:text-white "
-            >
-              <svg
+    <>
+      <section id="about">
+        <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center mt-10">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white mt-5 ">
+              Hi, my name is Andrew Alagna.
+            </h1>
+            <p className="mb-7 leading-relaxed">
+              A motivated student of computer science, interested in working with people
+              who want to solve meaningful problems with software engineering.
+              <br />
+              <br />
+              I enjoy learning about technology and find pride in solving problems.
+              <br />
+              <br />
+              Graduated from CUNY: Hunter College with a BA in Computer Science, Cum
+              Laude.
+            </p>
+            <ul className="flex lg:flex-row space-x-4 flex-row list-none mb-5">
+              <a
+                href="https://www.linkedin.com/in/andrew-a-10b88215b/"
+                rel="noreferrer"
+                target="_blank"
+                class="mb-6 inline-flex items-center h-10 p-3 text-gray-300 bg-gray-800 hover:bg-gray-700 transition-colors duration-500 rounded focus:shadow-outline hover:text-white  "
+              >
+                <svg
+                  class=" hover:text-white w-6 text-blue-500 fill-current duration-700 "
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
+                </svg>
+              </a>
+              <a
+                href="https://www.github.com/elchic00/"
+                rel="noreferrer"
+                target="_blank"
+                className="mb-6 inline-flex items-center h-10 p-3 text-gray-300 bg-gray-800 hover:bg-gray-700 duration-500 transition-colors rounded focus:shadow-outline hover:text-white "
+              >
+                <svg
                   aria-hidden="true"
                   focusable="false"
                   data-prefix="fab"
                   data-icon="github"
-                  className="svg-inline--fa fa-github w-6 text-white hover:text-white mr-3"
+                  className="svg-inline--fa fa-github w-6 text-white hover:text-white"
                   role="img"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 496 512"
-              >
-                <path
+                >
+                  <path
                     fill="currentColor"
                     d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
-                ></path>
-              </svg>
-              Visit my Github
-            </a>
-            <a
-              href="https://www.linkedin.com/in/andrew-a-10b88215b/"
-              rel="noreferrer"
-              target="_blank"
-              class="mb-6 inline-flex items-center h-10 p-5 text-gray-300 bg-gray-800 hover:bg-gray-700 transition-colors duration-500 rounded focus:shadow-outline hover:text-white  "
-            >
-              <svg
-                class=" hover:text-white mr-3 w-6 text-blue-500 fill-current duration-700 "
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-              >
-                <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
-              </svg>
-              Visit my Linkedin
-            </a>
-            <div className="text-center">
+                  ></path>
+                </svg>
+              </a>
               <button
-                className="inline-flex items-center text-white bg-green-800 hover:bg-green-700 duration-500 border-0 h-10 px-5 rounded focus:outline-none text-lg mb-5"
+                className="inline-flex items-center text-center text-white bg-green-800 hover:bg-green-700 duration-500 border-0 h-10 px-5 rounded focus:outline-none text-lg mb-5"
                 onClick={() => navigate("/snake")}
               >
                 Play Snake
               </button>
+            </ul>
+            <div className="flex justify-center">
+              <Link
+                to="#contact"
+                className="duration-500 inline-flex text-white bg-lime-700 border-0 py-2 px-6 focus:outline-none hover:bg-lime-600 rounded text-lg"
+              >
+                Work With Me
+              </Link>
+              <Link
+                to="#projects"
+                className="ml-4 inline-flex text-gray-300 bg-indigo-800 duration-500 hover:bg-indigo-700 border-0 py-2 px-6 focus:outline-none hover:text-white rounded text-lg"
+              >
+                See My Past Work
+              </Link>
             </div>
-          </ul>
-          <div className="flex justify-center">
-            <Link
-              to="#contact"
-              className="duration-500 inline-flex text-white bg-lime-700 border-0 py-2 px-6 focus:outline-none hover:bg-lime-600 rounded text-lg"
-            >
-              Work With Me
-            </Link>
-            <Link
-              to="#projects"
-              className="ml-4 inline-flex text-gray-300 bg-indigo-800 duration-500 hover:bg-indigo-700 border-0 py-2 px-6 focus:outline-none hover:text-white rounded text-lg"
-            >
-              See My Past Work
-            </Link>
           </div>
-        </div>
 
           <img
             className="object-cover object-center rounded mx-auto sm:h-80 h-80"
             alt="andrew"
             src="https://i.imgur.com/ddraBJx.jpg"
           />
-
-      </div>
-    </section>
+        </div>
+      </section>
 
       <Projects id="projects" />
       <Skills />
       {/* <Testimonials /> */}
       <Contact />
-      </>
-
+    </>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,40 +14,42 @@ export default function Projects() {
         </div>
         <div className="flex flex-wrap -m-4">
           {projects.map((project) => (
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={project.link}
-              key={project.image}
-              className="sm:w-1/2 w-100 p-4 z-0"
-            >
-              <div className="flex relative">
-                <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full sm:object-fill object-contain object-center"
-                  key={project.image}
-                  src={project.image}
-                />
-                <div className=" sm:px-8 sm:py-10 relative z-10 sm:w-full sm:border-4 sm:border-gray-800 bg-gray-900 opacity-0 sm:hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                    {project.subtitle}
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
-                    {project.title}
-                  </h1>
-                  <p className="leading-relaxed">{project.description}</p>
+            <>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={project.link}
+                key={project.image}
+                className="sm:w-1/2 w-100 p-4 z-0"
+              >
+                <div className="flex relative">
+                  <img
+                    alt="gallery"
+                    className="absolute inset-0 w-full h-full md:object-fill object-contain object-center hover:bg-gray-800"
+                    key={project.image}
+                    src={project.image}
+                  />
+                  <div className=" sm:px-8 sm:py-10 relative z-10 sm:w-full sm:border-4 sm:border-gray-800 bg-gray-900 opacity-0 sm:hover:opacity-100 ">
+                    <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                      {project.subtitle}
+                    </h2>
+                    <h1 className="title-font text-lg font-medium text-white mb-3">
+                      {project.title}
+                    </h1>
+                    <p className="leading-relaxed">{project.description}</p>
+                  </div>
                 </div>
+              </a>
+              <div id="project-desc" className="sm:hidden mt-2">
+                <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                  {project.subtitle}
+                </h2>
+                <h1 className="title-font text-lg font-medium text-white mb-3">
+                  {project.title}
+                </h1>
+                <p className="leading-relaxed">{project.description}</p>
               </div>
-              <div id='project-desc' className='sm:hidden mt-2'>
-              <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                {project.subtitle}
-              </h2>
-              <h1 className="title-font text-lg font-medium text-white mb-3">
-                {project.title}
-              </h1>
-              <p className="leading-relaxed">{project.description}</p>
-              </div>
-            </a>
+            </>
           ))}
         </div>
       </div>
